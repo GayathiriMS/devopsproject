30b01009a7e90cb9b4b6fa0a123f6f996abd4c5b
root <root@ip-172-31-89-200.ec2.internal>
2022-03-12 19:39:36 +0000

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Welcome to my Devops Project!')
+  response.send('This is second commit!')
 })
 
 app.listen(app.get('port'), function() {
